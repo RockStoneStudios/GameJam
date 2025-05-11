68a443dbbce0f3ddc6f6b3faef1e014994beb68f
Mgdz18 <mgdz_18@hotmail.com>
2025-05-11 17:47:27 -0500

    Test Button of globles UI

---
+++ Assets/UI/Scripts/GlobulesPool.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GlobulesPool : MonoBehaviour
+{
+
+    public Button buttonCañon;
+    public Button buttonEscudo;
+    public Button buttonLuchon;
+    private Button selectedButton;
+    private Color defaultColor  = Color.white;
+    private Color selectedColor = Color.red;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    
+    void Start()
+    {
+        buttonCañon.onClick.AddListener(() => onButtonClick(buttonCañon, "cañon"));
+        buttonEscudo.onClick.AddListener(() => onButtonClick(buttonEscudo, "Escudo"));
+        buttonLuchon.onClick.AddListener(() => onButtonClick(buttonLuchon, "luchon"));   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void onButtonClick(Button clickedButton, string name)
+    {
+        Debug.Log(name);
+        if (selectedButton != null)
+        {
+            setButtonColor(selectedButton, defaultColor);
+        }
+
+        // Set the new selected button and color it
+        selectedButton = clickedButton;
+        setButtonColor(selectedButton, selectedColor);
+
+    }
+
+    void setButtonColor(Button button, Color color)
+    {
+        // This sets the button's Image component color
+        var image = button.GetComponent<Image>();
+        if (image != null)
+        {
+            image.color = color;
+        }
+    }
+
+    
+}
